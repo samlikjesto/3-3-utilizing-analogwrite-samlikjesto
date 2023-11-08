1aa924d3227c15ba3d6140174298fef12f7fb0be
samlikjesto <145718624+samlikjesto@users.noreply.github.com>
2023-11-08 15:01:48 -0700

    Create 3-3-utilizing-analogwrite.ino

---
+++ 3-3-utilizing-analogwrite.ino
@@ -0,0 +1,21 @@
+void loop() {
+  analogWrite(light,255);
+  delay(1000);
+  analogWrite(light,102);
+  delay(1000);
+  analogWrite(light,0);
+  delay(1000);
+  analogWrite(light2,255);
+  delay(1000);
+  analogWrite(light2,102);
+  delay(1000);
+  analogWrite(light2,0);
+  delay(1000);
+  analogWrite(light3,255);
+  delay(1000);
+  analogWrite(light3,102);
+  delay(1000);
+  analogWrite(light3,0);
+  delay(1000);
+    
+}
